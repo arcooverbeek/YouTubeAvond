17f99fd490de98c0e4df638fdc8c917f6c4cc92b
Arco Overbeek <arcooverbeek@gmail.com>
2013-08-10 00:08:33 +0200

    i it commit

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,18 @@
+class LinksController < ApplicationController
+
+  def index
+    @links = Link.all
+  end
+  
+  def new
+    @link = Link.new
+  end
+  def create
+    @link = Link.new(params[:link])
+    if @link.save
+      redirect_to links_path
+    else
+      render :new
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youtubeavond::Application.config.secret_token = '9d0fc59a2902c2d845e3c462cc403139b4d8b6ac9e1676f7e1ecb12de79b71d8779c3eb2db975838da681036b0ad9f0b933f2da04e51b2b1002353869b82d62d'
